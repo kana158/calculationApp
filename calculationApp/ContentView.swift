226d9c0550551eda7e114e9af54306ebecd2d1fb
kana158 <reiyoutou@gmail.com>
2022-02-28 17:56:51 +0900

    Initial Commit

---
+++ calculationApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  calculationApp
+//
+//  Created by 西片健也 on 2022/02/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ calculationApp/calculationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculationAppApp.swift
+//  calculationApp
+//
+//  Created by 西片健也 on 2022/02/28.
+//
+
+import SwiftUI
+
+@main
+struct calculationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
